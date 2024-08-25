6db8b6f294418b43ebe2576d86da9a4f5d32ffba
ajasshaffi <83956432+ajasshaffi@users.noreply.github.com>
2024-08-25 11:13:53 +0530

    Update 5.py

--- Task5/5.py
+++ Task5/5.py
@@ -1,10 +1,4 @@
 #5.Write a program that finds the unique elements in a list using a set
 lst1=[1,2,3,4,2,3]
-lst2=[]
-
-for i in lst1:
-    if i not in lst2:
-        lst2.append(i)
-    else:
-        lst2.remove(i)
-print(lst2)
+set1=set(lst1)
+print(set1)
